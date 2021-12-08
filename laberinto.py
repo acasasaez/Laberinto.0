b7587f7fcaed4a08972960d6d86d0e7f8d744646
acasasaez <acasasae@myuax.com>
2021-12-08 22:12:38 +0100

    no sé cómo llamar a los comits

--- laberinto.py
+++ laberinto.py
@@ -1,8 +1,9 @@
+from math import inf
 import turtle
 ventana = turtle.Screen()
 ventana.bgcolor ("purple")
 ventana.title ("Laberinto")
-ventana.setup(5,5)
+ventana.setup(500,500)
 class Pen (turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -10,3 +11,21 @@ class Pen (turtle.Turtle):
         self.color("pink")
         self.penup()
         self.speed(0)
+
+laberinto = []
+laberinto_1 = ["XXXXX",
+               "XXXXX",
+               "XXXXX",
+               "XXXXX",
+               "XXXXX"]
+laberinto.append(laberinto_1)
+def iniciar_lab(laberinto):
+    for file in range (len(laberinto)):
+        for column in range(len(laberinto[file])):
+            letra_X = laberinto [file][column]
+            screen_x = -288 + (column * 4)
+            screen_y = 288 - (file * 4)
+            if letra_X == "X":
+                pen.goto (screen_x, screen_y)
+                pen.stamp()
+pen = Pen()
