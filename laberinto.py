234c145566d3721aeaeb2ea95121f5a4f1756736
acasasaez <acasasae@myuax.com>
2021-12-08 21:53:44 +0100

    según el hombre que me está ayudando he creado un objeto que me va a permitir dibujar en pantalla

--- laberinto.py
+++ laberinto.py
@@ -1,5 +1,12 @@
 import turtle
 ventana = turtle.Screen()
-ventana.bgcolor ("black")
+ventana.bgcolor ("purple")
 ventana.title ("Laberinto")
 ventana.setup(5,5)
+class Pen (turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("pink")
+        self.penup()
+        self.speed(0)
